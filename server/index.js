63076a59fdcf7b53463e683834ae2c1c4012fb7f
Alexander Dung Dung <65543943+Alexander8437@users.noreply.github.com>
2023-10-15 17:46:32 +0530

    server commit 3

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
+app.use(cors(
+    {
+        credentials: true
+    }
+));
+
+app.use(express.json())
 
 app.use('/posts', router)
 
